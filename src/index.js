c1668a8d2fedbdc8353ae3743f64d9ee467e5371
Amit Ramani <amit97ramani@gmail.com>
2024-01-15 21:35:03 +0530

    cloudinary support and user, video model added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import dotenv from "dotenv";
-import connectDB from "../db/index.js";
+import connectDB from "./db/index.js";
 import app from "./app.js";
 
 dotenv.config();
+
 connectDB()
     .then(() => {
         app.listen(process.env.PORT || 8000, () => {
